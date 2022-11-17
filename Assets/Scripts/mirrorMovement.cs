53203637c5c6c074dead2bb6ddab428ca922a072
TomeuGarcia <tomeu.garcia@enti.cat>
2022-11-17 09:32:38 +0100

    added constraints project

--- Assets/Scripts/mirrorMovement.cs
+++ Assets/Scripts/mirrorMovement.cs
@@ -6,7 +6,34 @@ public class mirrorMovement : MonoBehaviour
 {
     public GameObject original;
 	
-	void Update ()
+
+
+    [Header("Angle Constraint")]
+    public bool AngleConstraintActive;
+    public bool CancelTwist;
+    [Range(0.0f, 180.0f)]
+    public float maxAngle;
+
+    [Range(0.0f, 180.0f)]
+    public float minAngle;
+
+    public Transform parent;
+    public Transform child;
+
+
+    [Header("Plane Constraint")]
+    public bool PlaneConstraintActive;
+    public bool debugLines;
+
+    public Transform plane;
+
+    // To define how "strict" we want to be
+    private float threshold = 0.00001f;
+
+
+
+
+    void Update ()
     {
         transform.localRotation = original.transform.localRotation;
     }
